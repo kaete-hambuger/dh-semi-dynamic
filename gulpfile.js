bc318aeebaa76e5bc5a1a62dd95b217ee14ffdf7
Michael Czechowski <mail@dailysh.it>
2016-08-16 15:27:00 +0200

    added browser sync successfully

--- gulpfile.js
+++ gulpfile.js
@@ -20,11 +20,14 @@ var data = require('gulp-data');
 var rename = require('gulp-rename');
 var del = require('del');
 var watch = require('gulp-watch');
+var browserSync = require('browser-sync');
+var modRewrite = require('connect-modrewrite');
 
 // Add default task
 gulp.task('default', function(cb) {
   	runSequence(
 		'compileStaticHtml',
+    'browserSync',
 		'watch'
   	)}
 );
@@ -40,6 +43,26 @@ gulp.task('watch', function(cb) {
       .on('end', cb);
   });
 });
+
+// BrowserSync
+gulp.task('browserSync', function() {
+  browserSync({
+    server: {
+      baseDir: './www',
+      livereload: true,
+      middleware: [
+        // redirect html requests to handlebars out folder for development
+        modRewrite([
+          // redirect all html files
+          '^\/([^\/]*\.html)$ /$1',
+          // redirect root (http://localhost/)
+          '^/$ /index.html [L]'
+        ])
+      ]
+    }
+  });
+});
+
 
 // Compile dynamic handlebars templates to static HTML
 gulp.task('compileStaticHtml', function() {
@@ -68,6 +91,6 @@ gulp.task('compileStaticHtml', function() {
       }
 
     }))
-    .pipe(gulp.dest(config.handlebarsTarget));
-    //.on('end', browserSync.reload);
+    .pipe(gulp.dest(config.handlebarsTarget))
+    .on('end', browserSync.reload);
 });
